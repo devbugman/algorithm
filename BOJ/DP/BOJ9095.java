25c89593fbca0d681bcb3b6803578f032e1b9631
chanbokW <cksqhr4961@naver.com>
2022-08-18 21:48:13 +0900

    dp

---
+++ BOJ/DP/BOJ9095.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9095 {
+    
+    static int[] dp = new int[100001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 1 + 2 + 3 +1
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i = 4; i < 11; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+
+
+}
---
+++ BOJ/백트래킹/BOJ1759.java
@@ -0,0 +1,15 @@
+public class BOJ1759 {
+    
+    static int L,C;
+    static char[] map;
+    static char[] arr = new char[20];
+    static boolean[] visit = new boolean[10001];
+
+    public static void main(String[] args) {
+        
+    }
+
+    static void solve(){
+        
+    }
+}
