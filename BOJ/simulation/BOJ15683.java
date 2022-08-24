a788ead973375e7ba34b3542729cf706b3ea1238
chanbokW <cksqhr4961@naver.com>
2022-08-25 01:54:42 +0900

    [boj simulation 감시]
    - 후 드디어 1231!!

--- BOJ/simulation/BOJ15683.java
+++ BOJ/simulation/BOJ15683.java
@@ -2,18 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ15683 {
 
-    private static int n, m;
-    private static int[][] map;
-    private static boolean[][] visit;
-    private static int zeroCount = 0;
-    private static List<Cctv> cctvs;
-    private static int[] dir;
-    private static int answer;
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visit;
+    static ArrayList<SecurityCamera> cctvs;
+    static int cctvSize;
+    static int zeroCount;
+    static int minCount;
+    static int[] dir;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +21,6 @@ public class BOJ15683 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        answer = Integer.MAX_VALUE;
         cctvs = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
@@ -32,217 +31,198 @@ public class BOJ15683 {
                     zeroCount++;
                 }
                 if (map[i][j] > 0 && map[i][j] < 6) {
-                    cctvs.add(new Cctv(i, j, map[i][j]));
+                    cctvs.add(new SecurityCamera(i, j, map[i][j]));
                 }
             }
         }
-        // 0 은 빈칸 6은 벽 1 ~ 5 cctv 번호 경우의 수 구하기
-        // 1 상 하 좌 우 4가지
-        // 2 상하 좌우 2가지
-        // 3 좌상 우상 좌하 우하 4가지
-        // 4 좌상우 우상하 좌상하 우하좌 4가지
-        // 5 상하좌우 1가지
-        // 21845 * 8
+
+        cctvSize = cctvs.size();
         dir = new int[] { 0, 4, 2, 4, 4, 1 };
-        buildCctv(new int[cctvs.size()], 0);
-        System.out.println(answer);
+        minCount = Integer.MAX_VALUE;
+        changeDirection(new int[cctvSize], 0);
+        System.out.println(minCount);
+
     }
 
-    private static void buildCctv(int[] cases, int idx) {
-        // cctv 번호로 1 ,2 ,3, 4, 5
-        if (idx == cctvs.size()) {
-            findCctv(cases);
+    private static void changeDirection(int[] camera, int depth) {
+        if (depth == cctvSize) {
+            findCamera(camera);
             return;
         }
-        // 방향
-        for (int i = 1; i <= dir[cctvs.get(idx).value]; i++) {
-            // cases = [1, 1, 1, 1, 1] [2, 3, 4, 1, 4]
-            cases[idx] = i;
-            buildCctv(cases, idx + 1);
+        for (int i = 1; i <= dir[cctvs.get(depth).number]; i++) {
+            camera[depth] = i;
+            changeDirection(camera, depth + 1);
         }
     }
 
-    private static void findCctv(int[] cases) {
-        int deleteCount = zeroCount;
+    private static void findCamera(int[] camera) {
         visit = new boolean[n][m];
-        for (int i = 0; i < cctvs.size(); i++) {
-            Cctv cctv = cctvs.get(i);
-            switch (cctv.value) {
+        int cnt = zeroCount;
+        for (int i = 0; i < cctvSize; i++) {
+            SecurityCamera cctv = cctvs.get(i);
+            switch (cctv.number) {
                 case 1:
-                    deleteCount -= findCctvOne(cctv.x, cctv.y, cases[i]);
+                    cnt -= findCctvOne(cctv, camera[i]);
                     break;
                 case 2:
-                    deleteCount -= findCctvTwo(cctv.x, cctv.y, cases[i]);
+                    cnt -= findCctvTwo(cctv, camera[i]);
                     break;
                 case 3:
-                    deleteCount -= findCctvThree(cctv.x, cctv.y, cases[i]);
+                    cnt -= findCctvThree(cctv, camera[i]);
                     break;
                 case 4:
-                    deleteCount -= findCctvFour(cctv.x, cctv.y, cases[i]);
+                    cnt -= findCctvFour(cctv, camera[i]);
                     break;
                 case 5:
-                    deleteCount -= findCctvFive(cctv.x, cctv.y);
+                    cnt -= findCctvFive(cctv);
                     break;
             }
         }
-        answer = Math.min(answer, deleteCount);
+        minCount = Math.min(minCount, cnt);
     }
 
-    // cctv 윗방향
-    private static int up(int x, int y) {
-        int count = 0;
-        for (int i = x - 1; i >= 0; i--) {
-            if (map[i][y] == 6) {
+    private static int up(SecurityCamera cctv) {
+        int cnt = 0;
+        for (int i = cctv.x - 1; i >= 0; i--) {
+            if (map[i][cctv.y] == 6) {
                 break;
-            } 
-            if (map[i][y] == 0 && !visit[i][y]) {
-                visit[i][y] = true;
-                count++;
+            }
+            if (map[i][cctv.y] == 0 && !visit[i][cctv.y]) {
+                cnt++;
+                visit[i][cctv.y] = true;
             }
         }
+        return cnt;
+    }
+
+    private static int down(SecurityCamera cctv) {
+        int cnt = 0;
+        for (int i = cctv.x + 1; i < n; i++) {
+            if (map[i][cctv.y] == 6) {
+                break;
+            }
+            if (map[i][cctv.y] == 0 && !visit[i][cctv.y]) {
+                cnt++;
+                visit[i][cctv.y] = true;
+            }
+        }
+        return cnt;
+    }
+
+    private static int left(SecurityCamera cctv) {
+        int cnt = 0;
+        for (int i = cctv.y - 1; i >= 0; i--) {
+            if (map[cctv.x][i] == 6) {
+                break;
+            }
+            if (map[cctv.x][i] == 0 && !visit[cctv.x][i]) {
+                cnt++;
+                visit[cctv.x][i] = true;
+            }
+        }
+        return cnt;
+    }
+
+    private static int right(SecurityCamera cctv) {
+        int cnt = 0;
+        for (int i = cctv.y + 1; i < m; i++) {
+            if (map[cctv.x][i] == 6) {
+                break;
+            }
+            if (map[cctv.x][i] == 0 && !visit[cctv.x][i]) {
+                cnt++;
+                visit[cctv.x][i] = true;
+            }
+        }
+        return cnt;
+    }
+
+    private static int findCctvFive(SecurityCamera cctv) {
+        int count = 0;
+        count += up(cctv) + down(cctv) + left(cctv) + right(cctv);
         return count;
     }
 
-    // cctv 아래방향
-    private static int down(int x, int y) {
+    private static int findCctvFour(SecurityCamera cctv, int camera) {
         int count = 0;
-        for (int i = x + 1; i < n; i++) {
-            if (map[i][y] == 6) {
+        switch (camera) {
+            case 1:// 상 좌 우
+                count += up(cctv) + left(cctv) + right(cctv);
                 break;
-            }
-            if (map[i][y] == 0 && !visit[i][y]) {
-                visit[i][y] = true;
-                count++;
-            }
-        }
-        return count;
-    }
-
-    // 왼쪽방향
-    private static int left(int x, int y) {
-        int count = 0;
-        for (int i = y - 1; i >= 0; i--) {
-            if (map[x][i] == 6) {
+            case 2: // 우 상 하
+                count += right(cctv) + up(cctv) + down(cctv);
                 break;
-            }
-            if (map[x][i] == 0 && !visit[x][i]) {
-                count++;
-                visit[x][i] = true;
-            }
-        }
-        return count;
-    }
-
-    // 오른쪽 방향
-    private static int right(int x, int y) {
-        int count = 0;
-        for (int i = y + 1; i < m; i++) {
-            if (map[x][i] == 6) {
+            case 3: // 하 우 좌
+                count += down(cctv) + right(cctv) + left(cctv);
                 break;
-            } else if (map[x][i] == 0 && !visit[x][i]) {
-                count++;
-                visit[x][i] = true;
-            }
-        }
-        return count;
-    }
-
-    private static int findCctvOne(int x, int y, int idx) {
-        int count = 0;
-        switch (idx) {
-            case 1:// 상
-                count += up(x, y);
-                break;
-            case 2: // 하
-                count += down(x, y);
-                break;
-            case 3: // 좌
-                count += left(x, y);
-                break;
-            case 4:
-                count += right(x, y);
-                break;
-        }
-        return count;
-    }
-
-    private static int findCctvTwo(int x, int y, int idx) {
-        int count = 0;
-        switch (idx) {
-            case 1:
-                // 상 하
-                count += up(x, y) + down(x, y);
-                break;
-            case 2: // 좌 우
-                count += left(x, y) + right(x, y);
+            case 4: // 좌 하 상
+                count += left(cctv) + down(cctv) + up(cctv);
                 break;
         }
         return count;
     }
 
-    private static int findCctvThree(int x, int y, int idx) {
+    private static int findCctvThree(SecurityCamera cctv, int camera) {
         int count = 0;
-        switch (idx) {
+        switch (camera) {
             case 1:// 상 좌
-                count += up(x, y) + left(x, y);
+                count += up(cctv) + left(cctv);
                 break;
             case 2: // 상 우
-                count += up(x, y) + right(x, y);
+                count += up(cctv) + right(cctv);
                 break;
             case 3: // 하 우
-                count += down(x, y) + right(x, y);
+                count += down(cctv) + right(cctv);
                 break;
             case 4: // 하 좌
-                count += down(x, y) + left(x, y);
+                count += down(cctv) + left(cctv);
                 break;
         }
         return count;
     }
 
-    private static int findCctvFour(int x, int y, int idx) {
+    private static int findCctvTwo(SecurityCamera cctv, int camera) {
         int count = 0;
-        switch (idx) {
-            case 1:// 상 좌 우
-                count += up(x, y) + left(x, y) + right(x, y);
+        switch (camera) {
+            case 1:
+                // 상 하
+                count += up(cctv) + down(cctv);
                 break;
-            case 2: // 하 좌 우
-                count += down(x, y) + right(x, y) + left(x, y);
-                break;
-            case 3: // 좌 상 하
-                count += left(x, y) + right(x, y) + up(x, y);
-                break;
-            case 4: // 우 상 하
-                count += right(x, y) + up(x, y) + down(x, y);
+            case 2: // 좌 우
+                count += left(cctv) + right(cctv);
                 break;
         }
         return count;
     }
 
-    private static int findCctvFive(int x, int y) {
-        // 상 하 좌 우 4방향
+    private static int findCctvOne(SecurityCamera cctv, int camera) {
         int count = 0;
-        count += up(x, y) + down(x, y) + left(x, y) + right(x, y);
+        switch (camera) {
+            case 1:// 상
+                count += up(cctv);
+                break;
+            case 2: // 하
+                count += down(cctv);
+                break;
+            case 3: // 좌
+                count += left(cctv);
+                break;
+            case 4:
+                count += right(cctv);
+                break;
+        }
         return count;
     }
 
-    // private static boolean existSix(int x, int y) {
-    //     return map[x][y] == 6;
-    // }
+    static class SecurityCamera {
+        int x;
+        int y;
+        int number;
 
-    // // zero이면서 방문하지 않았다면
-    // private static boolean existZero(int x, int y) {
-    //     return map[x][y] == 0 && !visit[x][y];
-    // }
-
-    private static class Cctv {
-        int x; // cctv x좌표
-        int y; // cctv y좌표
-        int value; // cctv번호
-
-        Cctv(int x, int y, int value) {
+        SecurityCamera(int x, int y, int number) {
             this.x = x;
             this.y = y;
-            this.value = value;
+            this.number = number;
         }
     }
 }
